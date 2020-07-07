c0b0ddf2bf83af9d087731d7f450ec31127f7193
raehowe <hao930826@qq.com>
2020-07-07 12:20:07 +0800

    upload by RaeHowe at 2020.7.7

---
+++ 2020.7.6/exer2/main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by 朱云昊 on 2020/7/7.
+//
+
+#include "iostream"
+
+int main(){
+    std::cout << "Please enter two numbers: " << std::endl;
+
+    int a = 0, b = 0;
+
+    std::cin >> a >> b;
+
+    std::cout << "The result is: " << a * b << std::endl;
+}
