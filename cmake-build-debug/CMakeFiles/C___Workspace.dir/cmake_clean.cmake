efd0538edc6b28b65c81314e7a07f7b8460bc4c2
raehowe <hao930826@qq.com>
2020-07-07 12:08:47 +0800

    upload by RaeHowe at 2020.7.7

---
+++ cmake-build-debug/CMakeFiles/C___Workspace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Workspace.dir/main.cpp.o"
+  "C___Workspace"
+  "C___Workspace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Workspace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
